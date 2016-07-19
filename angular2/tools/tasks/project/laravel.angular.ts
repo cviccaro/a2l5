1f4ad439b9cb12a5a533cdd70f42b9f0cf729a7d
Chris Viccaro <cviccaro@jpenterprises.com>
2016-07-19 16:59:59 -0400

    refactored laravel.angular task using filter

--- angular2/tools/tasks/project/laravel.angular.ts
+++ angular2/tools/tasks/project/laravel.angular.ts
@@ -2,30 +2,42 @@ import * as gulp from 'gulp';
 import * as gulpLoadPlugins from 'gulp-load-plugins';
 import * as merge from 'merge-stream';
 import { join } from 'path';
-import { INJECTABLES, ENV, APP_DEST,  } from '../../config';
+import { INJECTABLES, ENV, APP_DEST } from '../../config';
 
 const plugins = <any>gulpLoadPlugins();
 
+/**
+ * For use when serving over PHP on a server
+ * using standard laravel nginx/apache config
+ *
+ * Moves requisite node modules into the public directory
+ *
+ * @returns {NodeJS.ReadWriteStream}
+ */
 function copyNodeModules() {
   return gulp.src(INJECTABLES, { cwd: 'node_modules/**' })
-    //.pipe(plugins.wait(2500))
     .pipe(gulp.dest('../public/node_modules'));
 }
 
+/**
+ * Copy built files and rename and move
+ * index.html to views folder
+ *
+ * @returns {NodeJS.ReadWriteStream}
+ */
 function copyBuiltFiles() {
+  const f = plugins.filter(`${APP_DEST}/index.html`, {restore: true});
+
   return gulp.src(join(APP_DEST, '**', '*'))
-   // .pipe(plugins.wait(1500))
+    .pipe(f)
+    .pipe(plugins.rename('angular.blade.php'))
+    .pipe(gulp.dest('../resources/views'))
+    .pipe(f.restore)
     .pipe(gulp.dest('../public'));
 }
 
-function createLaravelBlade() {
-  return gulp.src('../public/index.html')
-    .pipe(plugins.rename('angular.blade.php'))
-    .pipe(gulp.dest('../resources/views'));
-}
-
 let stream = ENV === 'prod' ?
-  merge(copyBuiltFiles(), createLaravelBlade())
-  : merge(copyNodeModules(), copyBuiltFiles(), createLaravelBlade());
+  copyBuiltFiles()
+  : merge(copyNodeModules(), copyBuiltFiles());
 
 export = () => stream;
